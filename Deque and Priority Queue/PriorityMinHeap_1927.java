038cf7527bb366912740c556f79a81de2a544a25
jeongjwon <76391160+jeongjwon@users.noreply.github.com>
2022-09-16 17:04:23 +0900

    2022.08.29

---
+++ Deque and Priority Queue/PriorityMinHeap_1927.java
@@ -0,0 +1,26 @@
+
+import java.util.*;
+import java.io.*;
+
+public class Baekjoon_PriorityMinHeap_1927 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>();
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int x = Integer.parseInt(br.readLine());
+            if (x != 0) {
+                int[] a = { Math.abs(x), x };
+                pq.add(a);
+            } else {
+                if (pq.isEmpty())
+                    System.out.println("0");
+                else
+                    System.out.println(pq.poll());
+            }
+        }
+    }
+}
