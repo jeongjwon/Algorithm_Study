be1c1d24e155e4b9549e7ce4276bfe4b59a770d2
jeongjwon <zlzlsksk111@gmail.com>
2023-03-22 17:09:45 +0900

    fix bug & add ques

---
+++ Leetcode/Leetcode_49_Group_Anagrams.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Leetcode_49_Group_Anagrams {
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> result = new ArrayList<>();
+        return result;
+        
+    }
+    public static void main(String[] args){
+        String[] strs = {"eat","tea","tan","ate","nat","bat"};
+        List<List<String>> answer = groupAnagrams(strs);
+
+    }
+}
