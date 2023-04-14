77c60abda5109081e666ee8616e463a00728d1f2
jeongjwon <zlzlsksk111@gmail.com>
2023-04-14 11:34:57 +0900

    20230414 17.전화 번호 문자 조합 done!

--- Leetcode/Leetcode_17_Letter_Combinations_of_a_Phone_Number.java
+++ Leetcode/Leetcode_17_Letter_Combinations_of_a_Phone_Number.java
@@ -1,28 +1,47 @@
 import java.util.*;
 public class Leetcode_17_Letter_Combinations_of_a_Phone_Number {
+    public static void backtracking( List<String> result, String digits, HashMap<Character, String> numbers, String str, int index){
+        if(index == digits.length()){
+            result.add(str);
+            return;
+        }
+        char c = digits.charAt(index);
+        String letters =  numbers.get(c);
+
+        //"23"
+       //["ad","ae","af","bd","be","bf","cd","ce","cf"]
+       
+       //letters = "abc"; / "def"
+       //index 0 1
+       //      a d 
+       //        e
+       //        f
+        //         b d
+
+        for(int i = 0; i < letters.length(); i++){
+            System.out.println(str+letters.charAt(i));
+            backtracking(result, digits, numbers,str+letters.charAt(i), index+1);
+
+        }
+
+    }
     public static List<String> letterCombinations(String digits) {
         List<String> result = new ArrayList<>();
-        HashMap<Character, List<Character>> numbers = new HashMap<>();
+        HashMap<Character, String> numbers = new HashMap<>();
+        
         
         char[] symbols = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '*', '0', '#'};
         String[] letters = {"", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz", "+", " ", "#"};
 
+
         for(int i = 0 ; i < symbols.length ; i++){
-
             char[] chars = letters[i].toCharArray();
-            List<Character> list = new ArrayList<>();
-            for(char c : chars) list.add(c);
-            numbers.put(symbols[i], list );
+            numbers.put(symbols[i], letters[i] );
         }
-
         System.out.println(numbers);
-
-        for(char c : digits.toCharArray()){
-            int n = numbers.get(c).size();
-            Character[] chars = numbers.get(c).toArray(new Character[n]);
-            boolean[] visited = new boolean[n];
-            
-        }
+        backtracking(result, digits, numbers ,"" ,0);
+        
+        
         return result;
     }
     public static void main(String[] args){
